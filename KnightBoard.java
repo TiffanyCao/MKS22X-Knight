4ea307a03b3777dddfff98a306dd17fc5d2f0c31
TiffanyCao <tcao00@stuy.edu>
2019-02-14 11:11:43 -0500

    added comments

--- KnightBoard.java
+++ KnightBoard.java
@@ -23,27 +23,28 @@ public class KnightBoard{
   }
 
   /**A method that clears the board and makes it all 0's
-*/
+  */
   public void empty(){
     for(int i = 0; i < board.length; i++){
       for(int y = 0; y < board[i].length; y++){
-      board[i][y] = 0;
-    }
+        board[i][y] = 0;
+      }
     }
   }
 
   /**A method that prints out the board
   *@return the board in string form
-  *blank boards display 0's as underscores
-  *you get a blank board if you never called solve or
-  *when there is no solution
+  *-blank boards display 0's as underscores
+  *-you get a blank board if you never called solve or when there is no solution
+  *-boards with more than or equal to ten squares will have two-digit numbers,
+  * so single-digit numbers will have a space before then
   */
   public String toString(){
     String result = "";
     if(board.length * board[0].length >= 10){ //if there are two-digit numbers
       for(int i = 0; i < board.length; i++){
         for(int y = 0; y < board[i].length; y++){
-          if(board[i][y] < 10) result += " " + board[i][y] + " ";
+          if(board[i][y] < 10) result += " " + board[i][y] + " "; //space in front of single-digit numbers
           if(board[i][y] >= 10) result += board[i][y] + " ";
           if(y == board[i].length - 1) result += "\n";
         }
@@ -59,153 +60,38 @@ public class KnightBoard{
     return result;
   }
 
+  /**A helping method that makes a move for the knight
+  *it marks the square on the board with the move number
+  *@param int r is the current row
+  *@param int c is the current column
+  *@param int count is the move number
+  *@return boolean false if the square is out of bounds or already has a number
+  *                true if a move is possible
+  */
   public boolean makeMove(int r, int c, int count){
     if(r < 0 || r >= board.length || c < 0 || c >= board[0].length) return false;
     if(board[r][c] != 0) return false;
     board[r][c] = count;
     return true;
   }
-    /*if(type == 0){
-      if((r + moves[0][0] >= 0 && r + moves[0][0] < board.length) &&
-         (c + moves[0][1] >= 0 && c + moves[0][1] < board[r].length) &&
-         board[r + moves[0][0]][c + moves[0][1]] == 0){
-        board[r + moves[0][0]][c + moves[0][1]] = count;
-        return true;
-      }
-    }
-    if(type == 1){
-      if((r + moves[1][0] >= 0 && r + moves[1][0] < board.length) &&
-         (c + moves[1][1] >= 0 && c + moves[1][1] < board[r].length) &&
-         board[r + moves[1][0]][c + moves[1][1]] == 0){
-        board[r + moves[1][0]][c + moves[1][1]] = count;
-        return true;
-      }
-    }
-    if(type == 2){
-      if((r + moves[2][0] >= 0 && r + moves[2][0] < board.length) &&
-         (c + moves[2][1] >= 0 && c + moves[2][1] < board[r].length) &&
-         board[r + moves[2][0]][c + moves[2][1]] == 0){
-        board[r + moves[2][0]][c + moves[2][1]] = count;
-        return true;
-      }
-    }
-    if(type == 3){
-      if((r + moves[3][0] >= 0 && r + moves[3][0] < board.length) &&
-         (c + moves[3][1] >= 0 && c + moves[3][1] < board[r].length) &&
-         board[r + moves[3][0]][c + moves[3][1]] == 0){
-        board[r + moves[3][0]][c + moves[3][1]] = count;
-        return true;
-      }
-    }
-    if(type == 4){
-      if((r + moves[4][0] >= 0 && r + moves[4][0] < board.length) &&
-         (c + moves[4][1] >= 0 && c + moves[4][1] < board[r].length) &&
-         board[r + moves[4][0]][c + moves[4][1]] == 0){
-        board[r + moves[4][0]][c + moves[4][1]] = count;
-        return true;
-      }
-    }
-    if(type == 5){
-      if((r + moves[5][0] >= 0 && r + moves[5][0] < board.length) &&
-         (c + moves[5][1] >= 0 && c + moves[5][1] < board[r].length) &&
-         board[r + moves[5][0]][c + moves[5][1]] == 0){
-        board[r + moves[5][0]][c + moves[5][1]] = count;
-        return true;
-      }
-    }
-    if(type == 6){
-      if((r + moves[6][0] >= 0 && r + moves[6][0] < board.length) &&
-         (c + moves[6][1] >= 0 && c + moves[6][1] < board[r].length) &&
-         board[r + moves[6][0]][c + moves[6][1]] == 0){
-        board[r + moves[6][0]][c + moves[6][1]] = count;
-        return true;
-      }
-    }
-    if(type == 7){
-      if((r + moves[7][0] >= 0 && r + moves[7][0] < board.length) &&
-         (c + moves[7][1] >= 0 && c + moves[7][1] < board[r].length) &&
-         board[r + moves[7][0]][c + moves[7][1]] == 0){
-        board[r + moves[7][0]][c + moves[7][1]] = count;
-        return true;
-      }
-    }
-    return false;
-    */
 
+  /**A helping method that removes a move for the knight
+  *it unmarks the square on the board with a 0
+  *@param int r is the current row
+  *@param int c is the current column
+  *@return boolean false if the square is out of bounds or doesn't have a number (is 0)
+  *                true if unmarking is possible
+  */
   public boolean undoMove(int r, int c){
     if(r < 0 || r >= board.length || c < 0 || c >= board[0].length) return false;
     if(board[r][c] == 0) return false;
     board[r][c] = 0;
     return true;
   }
-    /*if(type == 0){
-      if((r + moves[0][0] >= 0 && r + moves[0][0] < board.length) &&
-         (c + moves[0][1] >= 0 && c + moves[0][1] < board[r].length) &&
-         board[r + moves[0][0]][c + moves[0][1]] != 0){
-        board[r + moves[0][0]][c + moves[0][1]] = 0;
-        return true;
-      }
-    }
-    if(type == 1){
-      if((r + moves[1][0] >= 0 && r + moves[1][0] < board.length) &&
-         (c + moves[1][1] >= 0 && c + moves[1][1] < board[r].length) &&
-         board[r + moves[1][0]][c + moves[1][1]] != 0){
-        board[r + moves[1][0]][c + moves[1][1]] = 0;
-        return true;
-      }
-    }
-    if(type == 2){
-      if((r + moves[2][0] >= 0 && r + moves[2][0] < board.length) &&
-         (c + moves[2][1] >= 0 && c + moves[2][1] < board[r].length) &&
-         board[r + moves[2][0]][c + moves[2][1]] != 0){
-        board[r + moves[2][0]][c + moves[2][1]] = 0;
-        return true;
-      }
-    }
-    if(type == 3){
-      if((r + moves[3][0] >= 0 && r + moves[3][0] < board.length) &&
-         (c + moves[3][1] >= 0 && c + moves[3][1] < board[r].length) &&
-         board[r + moves[3][0]][c + moves[3][1]] != 0){
-        board[r + moves[3][0]][c + moves[3][1]] = 0;
-        return true;
-      }
-    }
-    if(type == 4){
-      if((r + moves[4][0] >= 0 && r + moves[4][0] < board.length) &&
-         (c + moves[4][1] >= 0 && c + moves[4][1] < board[r].length) &&
-         board[r + moves[4][0]][c + moves[4][1]] != 0){
-        board[r + moves[4][0]][c + moves[4][1]] = 0;
-        return true;
-      }
-    }
-    if(type == 5){
-      if((r + moves[5][0] >= 0 && r + moves[5][0] < board.length) &&
-         (c + moves[5][1] >= 0 && c + moves[5][1] < board[r].length) &&
-         board[r + moves[5][0]][c + moves[5][1]] != 0){
-        board[r + moves[5][0]][c + moves[5][1]] = 0;
-        return true;
-      }
-    }
-    if(type == 6){
-      if((r + moves[6][0] >= 0 && r + moves[6][0] < board.length) &&
-         (c + moves[6][1] >= 0 && c + moves[6][1] < board[r].length) &&
-         board[r + moves[6][0]][c + moves[6][1]] != 0){
-        board[r + moves[6][0]][c + moves[6][1]] = 0;
-        return true;
-      }
-    }
-    if(type == 7){
-      if((r + moves[7][0] >= 0 && r + moves[7][0] < board.length) &&
-         (c + moves[7][1] >= 0 && c + moves[7][1] < board[r].length) &&
-         board[r + moves[7][0]][c + moves[7][1]] != 0){
-        board[r + moves[7][0]][c + moves[7][1]] = 0;
-        return true;
-      }
-    }
-    return false;
-  }
-  */
 
+  /**A method that checks if the board is full or completed
+  *@return boolean
+  */
   public boolean isFull(){
     for(int i = 0; i < board.length; i++){
       for(int y = 0; y < board[i].length; y++){
@@ -215,6 +101,9 @@ public class KnightBoard{
     return true;
   }
 
+  /**A method that checks if the board is empty
+  *@return boolean
+  */
   public boolean isEmpty(){
     for(int i = 0; i < board.length; i++){
         for(int y = 0; y < board[i].length; y++){
@@ -227,18 +116,27 @@ public class KnightBoard{
 
   /**A method for solving a knight board and labels the placing
   *should work on boards where the number of squares is under 100
+  *@param int startRow
+  *@param int startCol
   *@return boolean if the board can be solved or not
+  *@throws IllegalArgumentException if the starting position is out of bounds
   */
   public boolean solve(int startRow, int startCol){
     if((startRow < 0 || startRow >= board.length) ||
        (startCol < 0 || startCol >= board[0].length)){
       throw new IllegalArgumentException();
     }
-    if(!isEmpty()) return false;
-    return solveH(startRow, startCol, 1);
+    if(!isEmpty()) return false; //if the board already has numbers
+    return solveH(startRow, startCol, 1); //calls to helper function
   }
 
-  public boolean solveH(int r, int c, int count){
+  /**A helper recursive method for solving a knight board and labels the placing
+  *@param int r
+  *@param int c
+  *@param int count is the move number
+  *@return boolean if the board can be solved or not
+  */
+  private boolean solveH(int r, int c, int count){
     if(isFull()) return true;
     for(int i = 0; i < moves.length; i++){
       if(makeMove(r, c, count)){
@@ -249,18 +147,5 @@ public class KnightBoard{
       }
     }
     return false;
-  }
-
-  public static void main(String[] args){
-
-    System.out.println("---Testing Solve---");
-    System.out.println("*printing 5X5 empty board:");
-    KnightBoard one = new KnightBoard(5, 5);
-    System.out.println(one);
-
-    System.out.println("\n*testing solve(4, 4)*");
-    System.out.println(one.solve(4, 4));
-    System.out.println(one);
-
   }
 }
