5e7e0a087306979c3b5e73f313b8a3c88fab7873
Tiffany <tcao00@stuy.edu>
2019-02-22 23:04:45 -0500

    changed from ArrayList<Integer> to ArrayList<int[]>

--- Optimize.java
+++ Optimize.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 public class Optimize{
   public int numMoves; //number of moves from this square
-  public ArrayList<Integer> move; //list of possible moves
+  public ArrayList<int[]> move; //list of possible moves
 
   public Optimize(){ //initialize the object
-    numMoves = 0; 
-    move = new ArrayList<Integer>();
+    numMoves = 0;
+    move = new ArrayList<int[]>();
   }
 }
