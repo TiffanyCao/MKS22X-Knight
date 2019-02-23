f091f1be36c54fa45c6297b0aa86903a1533c8ef
Tiffany <tcao00@stuy.edu>
2019-02-22 20:33:16 -0500

    created an object that stores the number of moves and list of moves from each square

---
+++ Optimize.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Optimize{
+  public int numMoves; //number of moves from this square
+  public ArrayList<Integer> move; //list of possible moves
+
+  public Optimize(){ //initialize the object
+    numMoves = 0; 
+    move = new ArrayList<Integer>();
+  }
+}
