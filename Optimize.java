e0b22b6582b88f9440cb37c4860869b217ccca54
Tiffany <tcao00@stuy.edu>
2019-02-24 20:26:57 -0500

    added a variable for the original number of moves

--- Optimize.java
+++ Optimize.java
@@ -1,11 +1,13 @@
 import java.util.*;
 
 public class Optimize{
+  public int optOriginal;
   public int numMoves; //number of moves from this square
   public ArrayList<int[]> move; //list of possible moves
 
   public Optimize(){ //initialize the object
     numMoves = 0;
+    optOriginal = 0;
     move = new ArrayList<int[]>();
   }
 }
