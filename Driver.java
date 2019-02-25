ebac23a1ff801cbaf88dcfb56cf7ad911c2df7ea
Tiffany <tcao00@stuy.edu>
2019-02-24 21:06:00 -0500

    added more test cases for optimization

--- Driver.java
+++ Driver.java
@@ -72,10 +72,13 @@ public class Driver{
     System.out.println("6x3 countSolutions(0, 0): " + six.countSolutions(0, 0));
     //took too long- System.out.println("7x10 countSolutions(0, 0): " + seven.countSolutions(0, 0));
     //took too long- System.out.println("8x4 countSolutions(0, 0): " + eight.countSolutions(0, 0));
-*/
+    */
+
+//FOR TESTING OPTIMIZATION
+
     four.reset();
     five.reset();
-    //System.out.println(five.countSolutions2(0, 0));
+
     System.out.println(four.optBoard());
     System.out.println(four.optOriginal());
     System.out.println(four);
@@ -112,13 +115,18 @@ public class Driver{
     System.out.println(four.optM(four.opt[0][0].move));
     four.makeMove2(2, 1, 3);
     System.out.println(four);
+    System.out.println(four.optOriginal());
     System.out.println(four.optM(four.opt[0][0].move));
+    System.out.println(four.optM(four.opt[3][3].move));
+    System.out.println(four.optM(four.opt[1][3].move));
 
     four.reset();
     System.out.println(four);
     System.out.println(four.optBoard());
     System.out.println(four.optM(four.opt[0][0].move));
     System.out.println(four.solve2(0, 0));
+    System.out.println(four);
+    four.solve(0, 0);
     System.out.println(four);
     System.out.println(four.optBoard());
     System.out.println(four.optM(four.opt[0][0].move));
@@ -127,7 +135,37 @@ public class Driver{
     System.out.println(five.optM(0, 0));
     System.out.println(five.solve2(0, 0));
     System.out.println(five);
+    five.solve(0, 0);
+    System.out.println(five);
     System.out.println(five.optBoard());
     System.out.println(five.optM(0, 0));
+
+
+/*
+    System.out.println("*printing 1x1:\n" + one);
+    System.out.println("*printing solve(0,0):\n" + one.solve2(0,0));
+    System.out.println(one);
+    System.out.println("*printing 2x4:\n" + two);
+    System.out.println("*printing solve(0,0):\n" + two.solve2(0,0));
+    System.out.println(two);
+    System.out.println("*printing 3x3:\n" + three);
+    System.out.println("*printing solve(0,0):\n" + three.solve2(0,0));
+    System.out.println(three);
+    System.out.println("*printing 4x6:\n" + four);
+    System.out.println("*printing solve(0,0):\n" + four.solve2(0,0));
+    System.out.println(four);
+    System.out.println("*printing 5x5:\n" + five);
+    System.out.println("*printing solve(0,0):\n" + five.solve2(0,0));
+    System.out.println(five);
+    System.out.println("*printing 6x3:\n" + six);
+    System.out.println("*printing solve(0,0):\n" + six.solve2(0,0));
+    System.out.println(six);
+    //System.out.println("*printing 7x10:\n" + seven);
+    //System.out.println("*printing solve(0,0):\n" + seven.solve2(0,0));
+    //System.out.println(seven);
+    System.out.println("*printing 8x4:\n" + eight);
+    System.out.println("*printing solve(0,0):\n" + eight.solve2(0,0));
+    System.out.println(eight);
+*/
   }
 }
