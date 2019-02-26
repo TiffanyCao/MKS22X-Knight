f2b818975af870e943e45a062110bdbfa6cbabc1
TiffanyCao <tcao00@stuy.edu>
2019-02-26 16:57:43 -0500

    tried testing count optimized

--- Driver.java
+++ Driver.java
@@ -1,5 +1,5 @@
 public class Driver{
-  public static void main(String[] args) {
+  public static void main(String[] args){
     /*KnightBoard k = new KnightBoard(5, 5);
     System.out.println("---Testing Solve---");
     System.out.println("*printing 5x5 board:");
@@ -72,7 +72,6 @@ public class Driver{
     System.out.println("6x3 countSolutions(0, 0): " + six.countSolutions(0, 0));
     //took too long- System.out.println("7x10 countSolutions(0, 0): " + seven.countSolutions(0, 0));
     //took too long- System.out.println("8x4 countSolutions(0, 0): " + eight.countSolutions(0, 0));
-    */
 
 //FOR TESTING OPTIMIZATION
 
@@ -140,8 +139,6 @@ public class Driver{
     System.out.println(five.optBoard());
     System.out.println(five.optM(0, 0));
 
-
-/*
     System.out.println("*printing 1x1:\n" + one);
     System.out.println("*printing solve(0,0):\n" + one.solve2(0,0));
     System.out.println(one);
@@ -167,5 +164,37 @@ public class Driver{
     System.out.println("*printing solve(0,0):\n" + eight.solve2(0,0));
     System.out.println(eight);
 */
+
+    for(int i = 0; i < 5; i++){
+      runTest(i);
+    }
+}
+    //testcase must be a valid index of your input/output array
+    public static void runTest(int i){
+
+    KnightBoard b;
+    int[]m =   {4,5,5,5,5};
+    int[]n =   {4,5,4,5,5};
+    int[]startx = {0,0,0,1,2};
+    int[]starty = {0,0,0,1,2};
+    int[]answers = {0,304,32,56,64};
+    if(i >= 0 ){
+      try{
+        int correct = answers[i];
+        b = new KnightBoard(m[i%m.length],n[i%m.length]);
+
+        int ans  = b.countSolutions(startx[i],starty[i]);
+
+        if(correct==ans){
+          System.out.println("PASS board size: "+m[i%m.length]+"x"+n[i%m.length]+" "+ans);
+        }else{
+          System.out.println("FAIL board size: "+m[i%m.length]+"x"+n[i%m.length]+" "+ans+" vs "+correct);
+        }
+      }catch(Exception e){
+        System.out.println("FAIL Exception case: "+i);
+
+      }
+    }
   }
+
 }
